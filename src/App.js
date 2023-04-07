d83217c02f2337cb71ceaba7bf1c9a80e508274b
Denise Schirmer <denise_liima@hotmail.com>
2023-04-07 13:59:29 -0300

    added functionalities

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 import { motion } from "framer-motion";
 
@@ -9,10 +10,19 @@ import capitu from "../src/images/capitu.jpg";
 const images = [fotoPrincipal, dom, marie, capitu];
 
 function App() {
+  const carousel = useRef();
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {}, []);
+  console.log(carousel.current?.scrollWidth, carousel.current?.offsetWidth);
   return (
     <div className="App">
-      <motion.div className="carousel">
-        <motion.div className="inner">
+      <motion.div className="carousel" whileTap={{ cursor: "grabbing" }}>
+        <motion.div
+          className="inner"
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+        >
           {images.map((image) => (
             <motion.div className="item" key={image}>
               <img src={image} alt="Texto alt" />
